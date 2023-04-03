5d1e9ba999af588f9735229c4fab8afdb5e3e800
vishwas1451 <vishwas1451@gmail.com>
2023-04-03 22:07:48 +0530

    oof

--- contracts/Educational_Records.sol
+++ contracts/Educational_Records.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.18;
 //SPDX-License-Identifier:MIT
 
-contract Election {
+contract Student_Record {
     struct student{
         string name;
         string srn;
@@ -9,14 +9,14 @@ contract Election {
         uint marks2;
         uint marks3;
         uint marks4;
+        uint marks5;
     }
     mapping(string => student) private information;
 
-    function setStudentInformation (string memory name, string memory srn, uint marks1, uint marks2, uint marks3, uint marks4) public {
-        information[srn] = student(name,srn,marks1,marks2,marks3,marks4);
+    function setStudentInformation (string memory name, string memory srn, uint marks1, uint marks2, uint marks3, uint marks4, uint marks5) public {
+        information[srn] = student(name, srn, marks1, marks2, marks3, marks4, marks5);
     }
     function getStudentInformation (string memory srn) external view returns(student memory){
         return information[srn];
     }
-
 }
