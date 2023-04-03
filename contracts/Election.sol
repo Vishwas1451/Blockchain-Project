ac766f14602141c8bbf27a398ff4f01c8e5cd4a3
vishwas1451 <vishwas1451@gmail.com>
2023-04-03 14:05:54 +0530

    second commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,23 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.8.18;
+//SPDX-License-Identifier:MIT
 
 contract Election {
-    string public candidateName;
+    struct student{
+        string name;
+        string srn;
+        string marks1;
+        string marks2;
+        string marks3;
+        string marks4;
+    }
+    mapping(string => student) private information;
 
-    function Election () public {
-        candidateName = "Candidate 1";
+    function setStudentInformation (string memory name, string memory srn, string memory marks1, string memory marks2, string memory marks3, string memory marks4) public {
+        information[srn] = student(name,srn,marks1,marks2,marks3,marks4);
+    }
+    function getStudentInformation (string memory srn) public view returns(string memory){
+        student memory x = information[srn];
+        return x.srn;
     }
 
-    function setCandidate (string _name) public {
-        candidateName = _name;
-    }
 }
