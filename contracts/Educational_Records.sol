b7bf73ee4e43b4813b2460f3a8e80a2c0dc2ee9e
Vishwa-Mehta <vishwamehta2002@gmail.com>
2023-04-11 22:15:17 +0530

    updated solidity contract
    Signed-off-by: Vishwa-Mehta <vishwamehta2002@gmail.com>

--- contracts/Educational_Records.sol
+++ contracts/Educational_Records.sol
@@ -11,12 +11,29 @@ contract Student_Record {
         uint marks4;
         uint marks5;
     }
+
+    struct details{
+        string stud_srn;
+        string password;
+    }
+
+    mapping(string => details) private SIGNUP;
+
+    function addUser(string memory stud_srn, string memory password) public {
+        SIGNUP[stud_srn] = details(stud_srn, password);
+    }
+
+    function getSignupDetails(string memory srn) external view returns(details memory) {
+        return SIGNUP[srn];
+    }
+
     mapping(string => student) private information;
 
     function setStudentInformation (string memory name, string memory srn, uint marks1, uint marks2, uint marks3, uint marks4, uint marks5) public {
         information[srn] = student(name, srn, marks1, marks2, marks3, marks4, marks5);
     }
-    function getStudentInformation (string memory srn) external view returns(student memory){
+
+    function getStudentInformation (string memory srn) external view returns(student memory) {
         return information[srn];
     }
 }
